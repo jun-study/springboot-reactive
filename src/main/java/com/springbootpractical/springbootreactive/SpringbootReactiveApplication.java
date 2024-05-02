0fef95905f0d4290aaa5996205415187a55cc18d
junhkang <junhkang91@gmail.com>
2024-05-02 19:52:58 +0900

    initial commit

---
+++ src/main/java/com/springbootpractical/springbootreactive/SpringbootReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpractical.springbootreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactiveApplication.class, args);
+	}
+
+}
